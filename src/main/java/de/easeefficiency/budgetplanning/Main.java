1cd140ae115cb6b34bdbb68d90a84a9eb554d4e4
Constantin Butzke <constantin.butzke@gmail.com>
2016-11-20 19:43:30 +0100

    First simple JavaFX App.
    Signed-off-by: Constantin Butzke <constantin.butzke@gmail.com>

--- src/main/java/de/easeefficiency/budgetplanning/Main.java
+++ src/main/java/de/easeefficiency/budgetplanning/Main.java
@@ -2,12 +2,20 @@ package de.easeefficiency.budgetplanning;
 
 import org.pmw.tinylog.Logger;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
+		launch(args);	
+	}
 
-		
-		
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Logger.info("Application start.");
+		primaryStage.setTitle("BUDGET PLANNING");
+		primaryStage.show();
 	}
 
 }
